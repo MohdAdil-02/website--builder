256f022b1ca64b4d730153700ea16ef8530484fd
MohdAdil-02 <mohdadilansari02@gmail.com>
2026-07-08 21:12:11 +0530

    feat: set up th e frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className="text-3xl font-bold text-center pt-20">
+        AI Code Generator
+      </h1>
+    </div>
+  )
+}
+
+export default App
